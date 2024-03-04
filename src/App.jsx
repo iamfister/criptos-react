4f3375dd88e8ab42298ceb70a58fbce650b7b152
Cristian Yair Gutierrez <fistergutierrez@gmail.com>
2024-03-04 13:27:04 -0600

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import styled from "@emotion/styled";
 import "./App.css";
-import ImagenCripto from './assets/img/imagen-criptos.png'
+import ImagenCripto from "./assets/img/imagen-criptos.png";
 
 const Contenedor = styled.div`
   max-width: 900px;
   margin: 0 auto;
   width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
 `;
 
 const Imagen = styled.img`
@@ -13,19 +18,36 @@ const Imagen = styled.img`
   width: 80%;
   margin: 100px auto 0 auto;
   display: block;
-`
+`;
 
 const Heading = styled.h1`
   font-family: "Lato", sans-serif;
   color: #fff;
+  text-align: center;
+  font-weight: 700px;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
 `;
 
 function App() {
   return (
     <>
       <Contenedor>
-        <Imagen src={ImagenCripto}/>
-        <Heading>Hola</Heading>
+        <Imagen src={ImagenCripto} />
+
+        <div>
+          <Heading>Cotiza Monedas al Instante</Heading>
+        </div>
       </Contenedor>
     </>
   );
